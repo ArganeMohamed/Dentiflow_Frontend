53687c9c92f617ad51780afb6020709eda03d5b2
tahaanz7 <tahaanz70@gmail.com>
2024-05-16 11:51:33 +0100

    testtest

--- dentiflow/src/App.vue
+++ dentiflow/src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Hello, World</h1>
+  <div>
+    <h1>Hello, World</h1>
+    <TestComp />
+  </div>
 </template>
+<script>
+import TestComp from "./views/TestComp.vue";
+
+export default {
+  components: { TestComp },
+};
+</script>
---
+++ dentiflow/src/views/TestComp.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>al 3arandassiiiin</h1>
+</template>
+<!-- <script>
+export default {
+  name: "TestComp",
+};
+</script> -->
