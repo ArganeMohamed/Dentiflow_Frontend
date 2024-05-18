fa7b5fdcfa0f39f621ecee4461045d86da331488
tahaanz7 <tahaanz70@gmail.com>
2024-05-18 22:37:03 +0100

    test

--- dentiflow/src/router/index.js
+++ dentiflow/src/router/index.js
@@ -1,36 +1,42 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Main from "../components/Main.vue";
-import Login from "../components/Accounts/Login.vue"
-import Patients from "../components/Lists/Patients.vue"
-import Api from "../components/Api.vue"
+import App from "../views/App.vue";
+import LoginView from "@/views/LoginView.vue";
+import AssistantView from "@/views/AssistantView.vue";
+import DirectorView from "@/views/DirectorView.vue";
+import MedecinView from "@/views/MedecinView.vue"; // Corrected the import
 
 const routes = [
-    {
-        path: '/',
-        name: 'Main',
-        component: Main
-    },
-    {
-        path:'/login',
-        name:'Login',
-        component:Login
-    },
-    {
-        path:'/patients',
-        name:'Patients',
-        component:Patients
-    },
-    {
-        path:'/api',
-        name:'api',
-        component:Api
-    }
-
-]
+  {
+    path: "/",
+    name: "App",
+    component: App,
+    /* redirect: "/login", // Redirects root path to /login */
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: LoginView,
+  },
+  {
+    path: "/assistant",
+    name: "Assistant",
+    component: AssistantView,
+  },
+  {
+    path: "/director",
+    name: "Director",
+    component: DirectorView,
+  },
+  {
+    path: "/medecin",
+    name: "Medecin",
+    component: MedecinView,
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
 });
 
 export default router;
